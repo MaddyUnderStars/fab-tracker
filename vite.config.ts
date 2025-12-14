7c17efd6e0daf5e3867a7efde68be653e4d03da8
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-12-14 18:15:52 +1100

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["health.png"],
+			manifest: {
+				name: "FaB Life Tracker",
+				short_name: "Life Tracker",
+				description: "Flesh and Blood Life Tracker",
+				theme_color: "#020618",
+				icons: [
+					{
+						src: "192.png",
+						sizes: "192x192",
+						type: "image/png"
+					}
+				]
+			}
+		})],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
