134af61a9d0aece187b49c7853db478e8f48bb97
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-12-16 21:30:55 +1100

    feat: disable back

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,14 @@ import { cacheGetHeroes } from "./lib/cards.ts";
 
 const HEROES = cacheGetHeroes();
 
+// disable browser back button
+// TODO: move to a router library instead of using modals
+// and handle back properly
+history.pushState(null, "", location.href);
+window.onpopstate = (event) => {
+	history.go(1);
+};
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
