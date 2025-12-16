42fddc8c17dcdff4a09dc4f5e5dd5a8da446da2b
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2025-12-16 22:01:14 +1100

    feat: ability to add multiple of the same hero

--- src/components/HeroList.tsx
+++ src/components/HeroList.tsx
@@ -1,3 +1,4 @@
+import { XIcon } from "lucide-react";
 import { useState } from "react";
 import type { Card } from "@/lib/cards";
 import { cn } from "@/lib/utils";
@@ -20,13 +21,15 @@ export const HeroSelectComponent = ({
 	const [selectedHeroes, setSelectedHeroes] = useState(selected);
 
 	const selectHero = (x: Card) => {
-		let ret = [...(selectedHeroes || [])];
+		const ret = [...(selectedHeroes || [])];
 
-		if (selectedHeroes?.find((y) => y.card_id === x.card_id)) {
-			ret = ret.filter((y) => y.card_id !== x.card_id);
-		} else {
-			ret.push({ card_id: x.card_id });
-		}
+		// if (
+		// selectedHeroes?.filter((y) => y.card_id === x.card_id).length === 1
+		// ) {
+		// ret = ret.filter((y) => y.card_id !== x.card_id);
+		// } else {
+		ret.push({ card_id: x.card_id });
+		// }
 
 		if (ret.length > 4) return;
 
@@ -34,31 +37,71 @@ export const HeroSelectComponent = ({
 		onClick(ret);
 	};
 
-	return (
-		<div className="grid gap-1 grid-cols-4 lg:grid-cols-8 text-center">
-			{heroes.map((currHero) => (
-				<button
-					key={currHero.card_id}
-					type="button"
-					onClick={() => selectHero(currHero)}
-					className={cn(
-						"p-1 cursor-pointer border-2",
-						selectedHeroes?.find(
-							(x) => x.card_id === currHero.card_id,
-						)
-							? "border-accent-foreground"
-							: "",
-					)}
-				>
-					<div>
-						<HeroComponent hero={currHero} />
-					</div>
+	const removeHero = (card_id: string) => {
+		const ret = [...(selectedHeroes || [])];
 
-					<span>
-						<div className="truncate">{currHero.name}</div>
-					</span>
-				</button>
-			))}
+		ret.splice(
+			ret.findIndex((x) => x.card_id === card_id),
+			1,
+		);
+
+		setSelectedHeroes(ret);
+		onClick(ret);
+	};
+
+	return (
+		<div className="grid gap-1 grid-cols-4 lg:grid-cols-8 text-center relative">
+			{heroes.map((currHero) => {
+				const duplicates =
+					selectedHeroes?.filter(
+						(x) => x.card_id === currHero.card_id,
+					) || [];
+
+				return (
+					<div
+						key={currHero.card_id}
+						className={cn(
+							"p-1 cursor-pointer border-2 relative",
+							selectedHeroes?.find(
+								(x) => x.card_id === currHero.card_id,
+							)
+								? "border-accent-foreground"
+								: "",
+						)}
+					>
+						{duplicates.length > 1 ? (
+							<span className="flex items-start justify-start absolute ps-1 top-0 left-0 z-10 bg-white text-black size-6 rounded-br-2xl">
+								{duplicates.length}
+							</span>
+						) : null}
+
+						{duplicates.length > 0 ? (
+							<button
+								type="button"
+								onClick={() => removeHero(currHero.card_id)}
+								className="flex items-start justify-end absolute top-0 right-0 z-10 bg-white size-6 rounded-bl-2xl text-red-800"
+							>
+								<XIcon size={20} />
+							</button>
+						) : null}
+
+						<button
+							type="button"
+							onClick={() => selectHero(currHero)}
+						>
+							<HeroComponent hero={currHero} />
+						</button>
+
+						<button
+							type="button"
+							onClick={() => selectHero(currHero)}
+							className="w-full truncate"
+						>
+							<span>{currHero.name}</span>
+						</button>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
