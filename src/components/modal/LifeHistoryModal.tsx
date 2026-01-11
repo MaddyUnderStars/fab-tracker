828bad0aa5c3ade365e774ec6c3e26ee0367a700
MaddyUnderStars <46743919+MaddyUnderStars@users.noreply.github.com>
2026-01-11 17:32:14 +1100

    feat: life history table. close #19. close #26

--- src/components/modal/LifeHistoryModal.tsx
+++ src/components/modal/LifeHistoryModal.tsx
@@ -1,26 +1,93 @@
 import NiceModal, { useModal } from "@ebay/nice-modal-react";
 import { X as CloseIcon } from "lucide-react";
 import { observer } from "mobx-react-lite";
-import type { Player } from "@/lib/player";
+import { settings } from "@/lib/settings";
 
 export const LifeHistoryModal = NiceModal.create(
-	observer(({ player }: { player: Player }) => {
+	observer(() => {
+		const players = settings.players;
 		const modal = useModal();
+
+		const mergedHistory = players
+			.flatMap((x) =>
+				x.history.slice(0, -1).map((y) => ({ ...y, player: x.id })),
+			)
+			.sort((a, b) => a.time - b.time);
 
 		return (
 			<div className="z-50 absolute top-0 left-0 bg-background text-foreground w-full h-full">
-				<div className="flex items-center justify-between bg-accent p-4 fixed w-full">
-					<h1>Life History for {player.hero.name}</h1>
+				<div className="flex items-center justify-between bg-secondary p-4 fixed w-full">
+					<h1>Life History</h1>
 					<button type="button" onClick={() => modal.remove()}>
 						<CloseIcon size={24} />
 					</button>
 				</div>
 
-				<div className="mt-16 flex items-center flex-col">
-					{player.history.map((life) => (
-						<p key={life.time}>{life.value}</p>
-					))}
-				</div>
+				<table className="mt-16 w-full table-fixed text-center">
+					<thead>
+						<tr>
+							{players.map((player) => (
+								<th scope="col" key={player.id}>
+									{player.hero.display_name}
+								</th>
+							))}
+						</tr>
+					</thead>
+					<tbody className="[&>tr]:even:bg-accent/25">
+						<tr>
+							{players.map((x) => (
+								<td key={x.id}>{x.hero.life}</td>
+							))}
+						</tr>
+
+						{mergedHistory.map((entry) => {
+							const playerIndex = players.findIndex(
+								(x) => x.id === entry.player,
+							);
+
+							const player = players[playerIndex];
+
+							const prev =
+								player.history[
+									player.history.findIndex(
+										(x) => x.time === entry.time,
+									) + 1
+								];
+							const delta = prev ? entry.value - prev.value : 0;
+
+							return (
+								<tr key={entry.time}>
+									{new Array(playerIndex)
+										.fill(0)
+										.map((_, i) => (
+											<td key={`${entry.time}${i}`}></td>
+										))}
+
+									<td>
+										{entry.value} (
+										<span
+											className={
+												delta > 0
+													? "text-green-500"
+													: "text-red-500"
+											}
+										>
+											{delta > 0 ? "+" : "-"}
+											{Math.abs(delta)}
+										</span>
+										)
+									</td>
+
+									{new Array(players.length - playerIndex - 1)
+										.fill(0)
+										.map((_, i) => (
+											<td key={`${entry.time}${i}`}></td>
+										))}
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
 			</div>
 		);
 	}),
